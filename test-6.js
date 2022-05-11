63b27ea79e5f4ccdf7993a6433b196ea993a50cd
Agels <ardhianagilr@gmail.com>
2022-05-11 17:56:29 +0700

    remove built in function

--- test-6.js
+++ test-6.js
@@ -2,6 +2,12 @@ const data = [1, 4, 7, 9, 12];
 const low = 2;
 const high = 15;
 
-const newData = data.filter((e) => e > low && e < high);
+let newArr = [];
+for (let i = 0; i < data.length; i++) {
+  if (data[i] > low && data[i] < high) {
+    newArr.push(data[i]);
+  }
+}
 
-console.log(newData);
+console.log(newArr);
+
--- test-7.js
+++ test-7.js
@@ -2,7 +2,12 @@ const data = [1, 4, 7, 9, 12];
 const low = 2;
 const high = 15;
 
-const newData = data.filter((e) => e > low && e < high);
-
-console.log(newData);
-console.log(newData.length);
+let newArr = [];
+let total = 0;
+for (let i = 0; i < data.length; i++) {
+  if (data[i] > low && data[i] < high) {
+    newArr.push(data[i]);
+    total++;
+  }
+}
+console.log(total);
